d7aae8e09192025d55fbc3966d700c84cc212ec1
Megan Collier <meganraec@live.com>
2018-03-23 03:23:31 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,9 +13,8 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -25,13 +24,11 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
-
+shipping_manifest["musket" = 2]
 
   # add 4 gun powder to the shipping_manifest hash below
-
-
+shipping_manifest["gun powder" => 4]
 
   # return the shipping_manifest hash below
-
+puts shipping_manifest
 end
